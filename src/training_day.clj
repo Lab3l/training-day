63267ee70fbfcac6188c5ea4f113c289f4573be2
Tuisku Salminen <tuisku.salminen@eficode.com>
2016-03-01 11:19:51 +0200

    rdy

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] (* x x))
 
-(defn average [a b]
-  ":(")
+(defn average [a b] (/(+ a b)2))
+
+hai
+(square 2)
+
+(average 2 2)
+(average 3 5)
+(average 2 1)
